80b5c44d8226757208a4bd19bee9aa544a4729c2
Artyom Pertsovsky <artyom@pertsovsky.com>
2025-01-17 19:16:07 +0200

    Move QK_BOOT to better place

--- keyboards/handwired/dactyl_cosmos/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_cosmos/keymaps/default/keymap.c
@@ -18,36 +18,36 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,         KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
-        KC_A,    KC_R,    KC_S,    KC_T,    KC_G,         KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
-        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,         KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_QUOT,
-                 KC_LBRC, KC_RBRC,                                          KC_MINS, KC_EQL,
+        KC_Q,    KC_W,    KC_F,    KC_P,     KC_B,         KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
+        KC_A,    KC_R,    KC_S,    KC_T,     KC_G,         KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
+        KC_Z,    KC_X,    KC_C,    KC_D,     KC_V,         KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_QUOT,
+                 KC_LBRC, KC_RBRC,                                           KC_MINS, KC_EQL,
 
-                          SFT_ESC, CTL_BSPC,KC_TAB,       KC_DEL,  ALT_SPC, SFT_ENT,
-                                   MO(1),   LSA(KC_NO),   KC_LGUI, OSL(2)
+                          SFT_ESC, CTL_BSPC, KC_TAB,       KC_DEL,  ALT_SPC, SFT_ENT,
+                                   MO(1),    LSA(KC_NO),   KC_LGUI, OSL(2)
     ),
     [1] = LAYOUT(
-        _______, M_PREV,  KC_MU,   M_NEXT,  _______,      _______, _______, KC_CAPS, _______, _______,
-        _______, KC_ML,   KC_MD,   KC_MR,   _______,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
-        _______, _______, _______, _______, _______,      KC_GRV,  KC_SLSH, KC_BSLS, KC_QUES, KC_PIPE,
-                 _______, _______,                                          KC_MB1,  KC_MB2,
-                          _______, _______, _______,      _______, _______, _______,
-                                   _______, _______,      _______, TO(3)
+        _______, M_PREV,  KC_MU,   M_NEXT,   _______,      _______, _______, KC_CAPS, _______, _______,
+        _______, KC_ML,   KC_MD,   KC_MR,    _______,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+        _______, _______, _______, _______,  _______,      KC_GRV,  KC_SLSH, KC_BSLS, KC_QUES, KC_PIPE,
+                 _______, _______,                                           KC_MB1,  KC_MB2,
+                          QK_BOOT, _______,  _______,      _______, _______, _______,
+                                   _______,  _______,      _______, TO(3)
     ),
     [2] = LAYOUT(
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-                 KC_F11,  KC_F12,                                           _______, _______,
-                          QK_BOOT, _______, _______,      _______, _______, _______,
-                                   _______, _______,      _______, _______
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+        KC_1,    KC_2,    KC_3,    KC_4,     KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+                 KC_F11,  KC_F12,                                            _______, _______,
+                          _______, _______,  _______,      _______, _______, _______,
+                                   _______,  _______,      _______, _______
     ),
     [3] = LAYOUT(
-        _______, KC_Q,    KC_W,    KC_E,    KC_R,         _______, _______, _______, _______, _______,
-        _______, KC_A,    KC_S,    KC_D,    KC_F,         _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______,
-                 _______, _______,                                          _______, _______,
-                          _______, _______, _______,      _______, _______, _______,
-                                   _______, _______,      _______, TO(0)
+        _______, KC_Q,    KC_W,    KC_E,     KC_R,         _______, _______, _______, _______, _______,
+        _______, KC_A,    KC_S,    KC_D,     KC_F,         _______, _______, _______, _______, _______,
+        _______, _______, _______, _______,  _______,      _______, _______, _______, _______, _______,
+                 _______, _______,                                           _______, _______,
+                          _______, _______,  _______,      _______, _______, _______,
+                                   _______,  _______,      _______, TO(0)
     )
 };
