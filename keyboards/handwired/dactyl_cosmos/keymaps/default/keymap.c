89cb948d3c9c8e6405f00054e6b08563730eca84
Artyom Pertsovsky <artyom@pertsovsky.com>
2025-02-11 00:43:18 +0200

    Improve qwerty

--- keyboards/handwired/dactyl_cosmos/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_cosmos/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_LBRC, KC_RBRC,                                           KC_MINS, KC_EQL,
 
                           SFT_ESC, CTL_BSPC, KC_TAB,       KC_DEL,  ALT_SPC, SFT_ENT,
-                                   MO(1),    LSA(KC_NO),   KC_LGUI, OSL(2)
+                                   MO(1),    LSA(KC_NO),   KC_LGUI, OSL(3)
     ),
     [1] = LAYOUT(
         _______, M_PREV,  KC_MU,   M_NEXT,   _______,      _______, _______, KC_CAPS, _______, _______,
@@ -32,22 +32,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______,  _______,      KC_GRV,  KC_SLSH, KC_BSLS, KC_QUES, KC_PIPE,
                  _______, _______,                                           KC_MB1,  KC_MB2,
                           QK_BOOT, _______,  _______,      _______, _______, _______,
-                                   _______,  _______,      _______, TO(3)
+                                   _______,  _______,      _______, TO(2)
     ),
     [2] = LAYOUT(
+        KC_ESC,  KC_Q,    KC_W,    KC_E,     KC_R,         _______, _______, _______, _______, _______,
+ KC_LEFT_SHIFT,  KC_A,    KC_S,    KC_D,     KC_F,         _______, _______, _______, _______, _______,
+        _______, _______, _______, KC_V,     KC_B,         _______, _______, _______, _______, _______,
+                 KC_T,    KC_G,                                              _______, _______,
+                    KC_LEFT_SHIFT, KC_SPC,   _______,      _______, _______, _______,
+                             KC_LEFT_CTRL,   MO(3),        _______, TO(0)
+    ),
+    [3] = LAYOUT(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
         KC_1,    KC_2,    KC_3,    KC_4,     KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
                  KC_F11,  KC_F12,                                            _______, _______,
                           _______, _______,  _______,      _______, _______, _______,
                                    _______,  _______,      _______, _______
-    ),
-    [3] = LAYOUT(
-        _______, KC_Q,    KC_W,    KC_E,     KC_R,         _______, _______, _______, _______, _______,
-        _______, KC_A,    KC_S,    KC_D,     KC_F,         _______, _______, _______, _______, _______,
-        _______, _______, _______, _______,  _______,      _______, _______, _______, _______, _______,
-                 _______, _______,                                           _______, _______,
-                          _______, _______,  _______,      _______, _______, _______,
-                                   _______,  _______,      _______, TO(0)
     )
 };
