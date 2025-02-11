536aa9dc8a215924b7fb170568cea06fc05c4f06
Artyom Pertsovsky <artyom@pertsovsky.com>
2025-02-11 22:57:25 +0200

    Add home, page down/up, end buttons and move caps

--- keyboards/handwired/dactyl_cosmos/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_cosmos/keymaps/default/keymap.c
@@ -27,10 +27,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    MO(1),    LSA(KC_NO),   KC_LGUI, OSL(3)
     ),
     [1] = LAYOUT(
-        _______, M_PREV,  KC_MU,   M_NEXT,   _______,      _______, _______, KC_CAPS, _______, _______,
-        _______, KC_ML,   KC_MD,   KC_MR,    _______,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
-        _______, _______, _______, _______,  _______,      KC_GRV,  KC_SLSH, KC_BSLS, KC_QUES, KC_PIPE,
-                 _______, _______,                                           KC_MB1,  KC_MB2,
+        _______, M_PREV,  KC_MU,   M_NEXT,   _______,      KC_HOME, KC_PAGE_DOWN, KC_PAGE_UP, KC_END,  KC_CAPS,
+        _______, KC_ML,   KC_MD,   KC_MR,    _______,      KC_LEFT, KC_DOWN,      KC_UP,      KC_RGHT, _______,
+        _______, _______, _______, _______,  _______,      KC_GRV,  KC_SLSH,      KC_BSLS,    KC_QUES, KC_PIPE,
+                 _______, _______,                                                KC_MB1,     KC_MB2,
                           QK_BOOT, _______,  _______,      _______, _______, _______,
                                    _______,  _______,      _______, TO(2)
     ),
@@ -43,11 +43,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_LEFT_CTRL,   MO(3),        _______, TO(0)
     ),
     [3] = LAYOUT(
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-        KC_1,    KC_2,    KC_3,    KC_4,     KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-                 KC_F11,  KC_F12,                                            _______, _______,
-                          _______, _______,  _______,      _______, _______, _______,
-                                   _______,  _______,      _______, _______
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+        KC_1,    KC_2,    KC_3,    KC_4,      KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,    KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+                 KC_F11,  KC_F12,                                             _______, _______,
+                          _______, _______, _______,        _______, _______, _______,
+                                   _______, _______,        _______, _______
     )
 };
